8d60393cf8349d80d0e9cf6c6ef7bf22445f68d8
Storm <storm.minecrafter.sss@gmail.com>
2026-06-24 19:36:14 -0400

    added dh exchange, generator generator, and brute force dh breaker

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ def mul(a,b,m):
         b=(b<<1)%m
     return r
 
+def pow(a,b,m):
+    b%=m-1
+    p=1
+    while b!=0:
+        if b%2==1:
+            p=mul(p,a,m)
+        b>>=1
+        a=mul(a,a,m)
+    return p
+
 def div(a,b,m):
     return mul(a,minv(b,m),m)
 
@@ -72,29 +82,45 @@ def verify(A,a,b,m):
 
 import cv2
 import numpy as np
+import random
+
+def find_secret_key(G,Key1,Key2,a,b,m):
+    A=G
+    c=1
+    while Key1!=A and Key2!=A:
+        A=add(A,G,a,b,m)
+        c+=1
+    if Key1==A:
+        print(scalar(Key2,c,a,b,m))
+    else:
+        print(scalar(Key1,c,a,b,m))
+
+def find_valid(a,b,m):
+    if m%4==3:
+        while True:
+            x=random.randint(0,m)
+            ex=(pow(x,3,m)+mul(a,x,m)+b)%m
+            if 1==pow(ex,m//2,m):
+                y=pow(ex,(m+1)//4,m)
+                if verify((x,y),a,b,m):
+                    return (x,y)
 
 def main():
-    a,b=2,3
-    p=17
-    for x in range(p):
-        for y in range(p):
-            G=(x,y)
-            if not verify(G,a,b,p):
-                continue
-            l=np.zeros((p,p),dtype=np.uint8)
-            brightness=200
-            l[*G]=255
-            A=G
-            c=1
-            while True:
-                A=add(A,G,a,b,p)
-                c+=1
-                if math.isinf(A[1]):
-                    break
-                brightness*=.8
-                l[*A]=int(brightness)+55
-            print(c)
-            cv2.imshow("test",cv2.resize(l,(800,800),interpolation=cv2.INTER_AREA))
-            cv2.waitKey(0)
+    a,b=124819,1234512
+    p=10000019
+    G=find_valid(a,b,p)
+    print(verify(G,a,b,p))
+    secret_key=random.randint(0,p)
+    my_key=random.randint(0,p)
+    what_you_send_me=scalar(G,secret_key,a,b,p)
+    print(what_you_send_me)
+    what_i_send_you=scalar(G,my_key,a,b,p)
+    print(what_i_send_you)
+    your_shared_key=scalar(what_i_send_you,secret_key,a,b,p)
+    my_shared_key=scalar(what_you_send_me,my_key,a,b,p)
+    print("Secret from onlookers")
+    print(your_shared_key)
+    print(my_shared_key)
+    find_secret_key(G,what_i_send_you,what_you_send_me,a,b,p)
 
 main()
